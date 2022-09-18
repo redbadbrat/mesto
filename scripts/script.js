16ebef02921f646ec12d0259c6f708b27c2fa653
Kseniia Telmanova <whoelse212@gmail.com>
2022-09-18 18:24:36 +0300

    added popup block, yet to set the style. added script.js

---
+++ scripts/script.js
@@ -0,0 +1,10 @@
+function openNameEditForm() {
+    document.getElementById("formNameEdit").style.display = "block";
+  }
+  
+function closeForm() {
+    document.getElementById("myForm").style.display = "none";
+  }
+
+alert("HIIIIIIIII THOMAS UWU");
+
